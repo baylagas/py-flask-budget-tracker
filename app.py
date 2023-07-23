dba75600df9a66cff9c7095e9bdc5d2df191ce87
balbino aylagas <balbino_a@hotmail.com>
2023-07-22 23:59:57 -0600

    added sql alchemy config

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@localhost:3306/budget_tracker_db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "horrible_secret_key"
+
+
+""" with app.app_context():
+    db.create_all() """
 
 
 @app.route("/")
